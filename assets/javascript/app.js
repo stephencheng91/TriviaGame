3888e5d3fcc0291bb3e6b3f5e5804db08f9c6492
KuanToto <stephencheng91@gmail.com>
2019-04-01 17:18:44 -0700

    rev -

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,8 @@
 
 //Create array to put questions.
 var questions = ["Cats are more populr than dogs in the US",
-    "Cats can be right-pawed or left-pawed", "Cats talk to each other by meowing",
+    "Cats can be right-pawed or left-pawed", 
+    "Cats talk to each other by meowing",
     "Chocolate is a special treat for cat",
     "Cats can usually squeeze their bodies through any space their head fits",
     "A cat's whiskers are about as long as her body is wide."];
@@ -10,36 +11,66 @@ var questions = ["Cats are more populr than dogs in the US",
 //true //true false false true true
 //Create array to put answer selections.
 var answerOptions = ["True", "False"];
-var answer = false;
+var answer;
 var correct = 0;
 var wrong = 0;
+var noAnswered = 0;
 
 var intervalId;
 var number = 10;
+var num = 0;
 
-function showQuestions(arry) {
+
+// start the game (clean the html), restart the varibles, send the first question and set the timer
+// in the decrement funtion:decrese counter, look if the counter is 0, if is 0: tell timeout, increase the index for next question,  and show next question
+
+function showQuestions(str) {
     $("#Questions").empty();
-    $("#Questions").append(arry);
+    $("#Questions").append(str);
 }
 
 //Setting up buttons for selections
-function showOptions(arry) {
+function showOptions() {
 
     $("#True").empty();
     $("#False").empty();
-    $("#True").append("<button>" + arry[0] + "</button>");
-    $("#False").append("<button>" + arry[1] + "</button>");
+    $("#True").show();
+    $("#False").show();
+    $("#True").append("<button>" + answerOptions[0] + "</button>");
+    $("#False").append("<button>" + answerOptions[1] + "</button>");
+    selectedAnswser();
 
+}
+
+function selectedAnswser() {
     $("#True").click(function () {
         answer = true;
         $("#True").hide();
         $("#False").hide();
+        if (num > questions.length-1) {
+            stop();
+            $("#message").text("Game Over");
+        }
+        setTimeout(function () {
+            num++;
+            $("#message").empty();
+            GameStart();
+        }, 2 * 1000)
     });
 
     $("#False").click(function () {
         answer = false;
         $("#True").hide();
         $("#False").hide();
+        if (num > questions.length-1) {
+            stop();
+            $("#message").text("Game Over");
+        }
+        setTimeout(function () {
+            num++;
+            $("#message").empty();
+            GameStart();
+        }, 2 * 1000)
     });
 }
 
@@ -48,21 +79,38 @@ function run() {
     clearInterval(intervalId);
     //show time decrease in 1 second.
     intervalId = setInterval(decrement, 1000);
+    if (num > questions.length-1) {
+        stop();
+        $("#message").text("Game Over");
+    }
 }
 function decrement() {
     number--;
     $("#Timer").html("<h2>" + number + "</h2>");
     if (number === 0) {
         stop();
+        noAnswered++
+        $("#message").text("Time's up");
+        // send a message telling timeout (wait 2 seconds and send the next question)      
+        setTimeout(function () {
+            num++;
+            $("#message").empty();
+            GameStart();
+        }, 2 * 1000)
     }
+
 }
+
 function stop() {
     clearInterval(intervalId);
 }
 
-function GameStart(num) {
+function GameStart() {
+    stop();
+    number = 10;
+    run();
     showQuestions(questions[num]);
-    showOptions(answerOptions);
+    showOptions();
 }
 
 
@@ -70,21 +118,8 @@ function GameStart(num) {
 $("#StartButton").click(function () {
 
     $("#StartButton").hide();
-
-    for (var i = 0; i < questions.length; i++) {
-        run();
-        GameStart(i);
-        //Setting time out for 10 seconds
-        
-        if (i === 0 && answer === true) {
-            correct++;
-        } else if (i === 0 && answer === false) {
-            wrong++;
-        }
-        $("#correct").text("Correct: " + correct);
-        $("#wrong").text("Wrong: " + wrong);
-
-    }
+    GameStart();
+    run();
 
 });
 
